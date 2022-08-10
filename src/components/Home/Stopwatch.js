18e71b2c7303d808484e4357bf3b443b2d1a277a
frostycakes <lamquoc.24@gmail.com>
2022-08-10 15:14:46 -0400

    Quoc Pull

--- src/components/Home/Stopwatch.js
+++ src/components/Home/Stopwatch.js
@@ -1,25 +1,29 @@
-import React from 'react';
+import React from "react";
 
-
-
-function MyStopwatch({setFishTime, minutes, seconds, isRunning, start, pause, reset}) {
-
-
- 
-
+function MyStopwatch({
+  setFishTime,
+  minutes,
+  seconds,
+  isRunning,
+  start,
+  pause,
+  reset,
+}) {
   return (
-    <div hidden style={{textAlign: 'center'}}>
-      <h1>react-timer-hook</h1>
-      <p>Stopwatch Demo</p>
-      <div style={{fontSize: '100px'}}>
-        <span>{seconds}</span>
+    <div hidden style={{ textAlign: "center" }}>
+      <div className="quoc">
+        <h1>react-timer-hook</h1>
+        <p>Stopwatch Demo</p>
+        <div style={{ fontSize: "100px" }}>
+          <span>{seconds}</span>
+        </div>
+        <p>{isRunning ? "Running" : "Not running"}</p>
+        <button onClick={start}>Start</button>
+        <button onClick={pause}>Pause</button>
+        <button onClick={reset}>Reset</button>
       </div>
-      <p>{isRunning ? 'Running' : 'Not running'}</p>
-      <button onClick={start}>Start</button>
-      <button onClick={pause}>Pause</button>
-      <button onClick={reset}>Reset</button>
     </div>
   );
 }
 
-export default MyStopwatch
+export default MyStopwatch;
